6f197d6835906744f732ae104bbb1a5bd4629a7b
Mats Olsberg <mats.olsberg@evry.com>
2020-12-08 10:48:59 +0100

    Add day 7 and 8

--- day-1/part-2.js
+++ day-1/part-2.js
@@ -201,12 +201,22 @@ const list = [
   1544,
 ]
 
+console.time('count')
 const listLength = list.length
 let i, j, k
+list.sort((a, b) => {
+  return a - b
+})
 for (i = 0; i < listLength; i += 1) {
+  if (i > 0 && list[i] === list[i - 1]) continue
   for (j = 0; j < listLength; j += 1) {
+    if (list[i] + list[j] >= 2020) break
+    if (j > 0 && list[j] === list[j - 1]) continue
     for (k = 0; k < listLength; k += 1) {
-      if (list[i] + list[j] + list[k] === 2020) {
+      const sum = list[i] + list[j] + list[k]
+      if (sum > 2020) break
+      if (sum === 2020) {
+        console.timeEnd('count')
         console.log(list[i] * list[j] * list[k])
         return
       }
